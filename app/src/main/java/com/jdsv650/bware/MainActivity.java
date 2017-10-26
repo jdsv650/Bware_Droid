87a05212dc6cfe4dd1d41cfeaca26b3ffeb6862b
James Donner <jdsv650@yahoo.com>
2017-10-25 21:12:31 -0400

    Add Maps Activity

--- app/src/main/java/com/jdsv650/bware/MainActivity.java
+++ app/src/main/java/com/jdsv650/bware/MainActivity.java
@@ -1,13 +1,30 @@
 package com.jdsv650.bware;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String PREFS_NAME = "PREFS";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // get shared prefs
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
+        String token = preferences.getString("token","");
+
+        // NO call to setContentView just start activity based on if we have a stored token
+        Intent intent;
+
+        if (token == "") { intent = new Intent(this, WelcomeActivity.class); }
+        else { intent = new Intent(this, MapsActivity.class); }
+
+        startActivity(intent);
+        finish();
+
     }
 }
--- app/src/main/java/com/jdsv650/bware/MapsActivity.java
+++ app/src/main/java/com/jdsv650/bware/MapsActivity.java
@@ -1,5 +1,11 @@
 package com.jdsv650.bware;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,10 +15,20 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import okhttp3.OkHttpClient;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    public static final int REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted = false;
+    LocationManager locManager;
+    private static final Double fiveMilesInMeters = 8046.72;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +38,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        OkHttpClient client = new OkHttpClient();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +61,124 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // get location
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        getLocationPermission();
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(43, -112);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        //mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        updateLocationUI();
+
+
+
+    }
+
+    private void getLocationPermission() {
+    /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_ACCESS_FINE_LOCATION);
+            mLocationPermissionGranted = false;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_ACCESS_FINE_LOCATION : {
+                // request cancelled, result array is empty
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // OK
+                    mLocationPermissionGranted = true;
+
+
+
+
+                } else {
+
+                    Toast.makeText(this,"", Toast.LENGTH_LONG).show();
+                    mLocationPermissionGranted = false;
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+
+    }
+
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+
+
+            if (mLocationPermissionGranted == true) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                mMap.addMarker(new MarkerOptions().position(new LatLng(43.171395, -78.679584)).title("Marker in Lockport"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.171395, -78.679584), 14)); // 14094
+
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+
+        Toast.makeText(this, "Location: Lat = " + location.getLatitude() + "  Lon = " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+        Toast.makeText(this, "Getting updates ...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+        Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show();
+
     }
 }
