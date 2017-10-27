38b225bf75d9bf6f6f283245034cbbe06bce32c2
James Donner <jdsv650@yahoo.com>
2017-10-26 20:13:33 -0400

    Add login pressed handler; pre Android Studio 3.0

--- app/src/main/java/com/jdsv650/bware/LoginActivity.java
+++ app/src/main/java/com/jdsv650/bware/LoginActivity.java
@@ -2,6 +2,9 @@ package com.jdsv650.bware;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +13,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+    public void loginPressed(View view)
+    {
+        EditText emailText = (EditText) findViewById(R.id.emailEditText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordEditText);
+        if (emailText.getText().toString().length() == 0 ||
+                passwordText.getText().toString().length() == 0)
+        {
+            Toast.makeText(this, "Email and password field must not be blank", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            // have creds so try for token
+            getToken();
+
+        }
+
+    }
+
+    void getToken()
+    {
+
+    }
+
+
 }
--- app/src/main/java/com/jdsv650/bware/MainActivity.java
+++ app/src/main/java/com/jdsv650/bware/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); 
+        super.onCreate(savedInstanceState);
 
         // get shared prefs
         SharedPreferences preferences = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
