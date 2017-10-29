5af559439bed4c12bce05f3d706114e9ee16c1c8
James Donner <jdsv650@yahoo.com>
2017-10-29 16:12:01 -0400

    get token from api on login page

---
+++ app/src/main/java/com/jdsv650/bware/Constants.java
@@ -0,0 +1,13 @@
+package com.jdsv650.bware;
+
+/**
+ * Created by james on 10/26/17.
+ */
+
+public class Constants {
+
+
+    public static final String baseUrlAsString = "http://www.bwaremap.com";
+    public static final String siteName = "";  //"/Bware"
+
+}
--- app/src/main/java/com/jdsv650/bware/LoginActivity.java
+++ app/src/main/java/com/jdsv650/bware/LoginActivity.java
@@ -1,12 +1,43 @@
 package com.jdsv650.bware;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity {
+
+
+    EditText emailText;
+    EditText passwordText;
+    final OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +48,10 @@ public class LoginActivity extends AppCompatActivity {
 
     public void loginPressed(View view)
     {
-        EditText emailText = (EditText) findViewById(R.id.emailEditText);
-        EditText passwordText = (EditText) findViewById(R.id.passwordEditText);
+        emailText = (EditText) findViewById(R.id.emailEditText);
+        passwordText = (EditText) findViewById(R.id.passwordEditText);
+
+
         if (emailText.getText().toString().length() == 0 ||
                 passwordText.getText().toString().length() == 0)
         {
@@ -26,17 +59,126 @@ public class LoginActivity extends AppCompatActivity {
         }
         else
         {
-            // have creds so try for token
-            getToken();
+            //Toast.makeText(this, "try login", Toast.LENGTH_SHORT).show();
 
+            // supplied creds so try for token
+           getToken();
         }
 
     }
 
-    void getToken()
+    public static final MediaType MEDIA_TYPE = MediaType.parse("application/json");
+
+
+    public void getToken()
     {
+        String urlAsString = Constants.baseUrlAsString + "/token";
+
+        Log.w("username", emailText.getText().toString());
+        Log.w("password", passwordText.getText().toString());
+
+        String reqBody = "grant_type=password&username=" + emailText.getText().toString()
+                        + "&password=" + passwordText.getText().toString() ;
+
+        //grant_type=password&username=jdsv650%40yahoo.com&password=******";
+
+        final String ALLOWED_URI_CHARS = "@#&=*+-_.,:!?()/~'%";
+        String urlEncoded = Uri.encode(reqBody, ALLOWED_URI_CHARS);
+
+        RequestBody body = RequestBody.create(MEDIA_TYPE, urlEncoded);
+
+        final Request request = new Request.Builder()
+                .url(urlAsString)
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        OkHttpClient trustAllclient = trustAllSslClient(client);
+
+        trustAllclient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+                //call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String mMessage = response.body().string();
+                Log.w("success Response", mMessage);
+
+
+                if (response.isSuccessful()){
+                    try {
+                        JSONObject json = new JSONObject(mMessage);
+
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                Toast.makeText(getBaseContext(), mMessage, Toast.LENGTH_LONG).show();
+                                //final String serverResponse = json.getString("");
+                            }
+                        });
+
+
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
 
     }
 
+
+    /****** SSL bypass /*
+     * This is very bad practice and should NOT be used in production.
+     */
+
+
+    private static final TrustManager[] trustAllCerts = new TrustManager[] {
+            new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[]{};
+                }
+            }
+    };
+    private static final SSLContext trustAllSslContext;
+    static {
+        try {
+            trustAllSslContext = SSLContext.getInstance("SSL");
+            trustAllSslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static final SSLSocketFactory trustAllSslSocketFactory = trustAllSslContext.getSocketFactory();
+
+    public static OkHttpClient trustAllSslClient(OkHttpClient client) {
+
+        OkHttpClient.Builder builder = client.newBuilder();
+        builder.sslSocketFactory(trustAllSslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+        builder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+        return builder.build();
+    }
 
 }
