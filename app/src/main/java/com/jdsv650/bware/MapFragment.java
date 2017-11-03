616a2881f0b42a02057a430ae8e7681002410de1
James Donner <jdsv650@yahoo.com>
2017-11-03 00:06:57 -0400

    add API (get) call to retrieve bridge info Okhttp)

--- app/src/main/java/com/jdsv650/bware/MapFragment.java
+++ app/src/main/java/com/jdsv650/bware/MapFragment.java
@@ -2,10 +2,13 @@ package com.jdsv650.bware;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -28,9 +31,34 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.Map;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.MediaType;
+import okhttp3.Response;
+
+
+import static android.content.Context.MODE_PRIVATE;
+import static com.jdsv650.bware.Constants.PREFS_NAME;
 
 
 /**
@@ -45,8 +73,17 @@ public class MapFragment extends Fragment implements LocationListener {
     public static final int REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean mLocationPermissionGranted = false;
     LocationManager locManager;
-    private static final Double fiveMilesInMeters = 8046.72;
-    private static final Integer thirtySeconds = 30000;
+    private static final Integer fiveMilesInMeters = 8047;
+    private static final Integer sixtSeconds = 60000;
+    public static final MediaType MEDIA_TYPE = MediaType.parse("application/json");
+
+
+    Double geographicCenterUSLat = 39.833333;
+    Double geographicCenterUSLon = -98.583333;
+    Integer numMilesToSearch = 50;  // can be changed by preference
+
+    OkHttpClient client = new OkHttpClient();
+
 
 
     public MapFragment() {
@@ -119,6 +156,7 @@ public class MapFragment extends Fragment implements LocationListener {
 
        // Toast.makeText(getActivity(), "Location: Lat = " + location.getLatitude() + "  Lon = " + location.getLongitude(), Toast.LENGTH_SHORT).show();
         updateLocationUI(location.getLatitude(), location.getLongitude());
+        getBridgeData(50);
 
     }
 
@@ -174,8 +212,8 @@ public class MapFragment extends Fragment implements LocationListener {
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mLocationPermissionGranted = true;
-            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, thirtySeconds, 0, this);
-            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, thirtySeconds, 0, this);
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, fiveMilesInMeters, this);
+            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, fiveMilesInMeters, this);
 
         } else {
             ActivityCompat.requestPermissions(getActivity(),
@@ -235,7 +273,273 @@ public class MapFragment extends Fragment implements LocationListener {
         }
     }
 
+    private void getBridgeData(Integer miles)
+    {
+        String urlAsString = Constants.baseUrlAsString + "/api/Bridge/GetByMiles?lat=43.112615&lon=-78.795&miles=50";
 
+        // get shared prefs
+        SharedPreferences preferences = getActivity().getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
+        String token = preferences.getString("access_token","");  // is token stored
+
+        if (token != "")
+        {
+            String urlEncoded = Uri.encode(urlAsString);
+            RequestBody body = RequestBody.create(MEDIA_TYPE, urlEncoded);
+
+
+            Request request = new Request.Builder()
+                    .url(urlAsString)
+                    .addHeader("Authorization", "Bearer " + token)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    String mMessage = e.getMessage().toString();
+                    Log.w("failure Response", mMessage);
+                    //call.cancel();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    final String mMessage = response.body().string();
+                    Log.w("success Response", mMessage);
+
+                    if (response.isSuccessful()){
+                        try {
+                           // final JSONObject json = new JSONObject(mMessage);
+                            final JSONArray jsonArray = new JSONArray(mMessage);
+
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    // Toast.makeText(getBaseContext(), mMessage, Toast.LENGTH_LONG).show();
+                                    try {
+
+                                        drawMap(jsonArray);
+
+                                        /***
+                                         String err = json.getString("error");
+
+                                         if (err == "invalid_grant")
+                                         {
+                                         Toast.makeText(getBaseContext(), "Please verify your email and password", Toast.LENGTH_SHORT).show();
+                                         return;  // don't try to get token info it failed so just exit
+                                         }  ****/
+
+                                       // String accessToken = json.getString("access_token");
+                                       // String expires = json.getString(".expires");
+                                       // String username = json.getString("userName");
+
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Toast.makeText(getActivity(), "Error logging in please try again", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                            // go to map
+                            Intent intent = new Intent(getActivity(), BottomNavigationActivity.class);
+                            startActivity(intent);
+
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                    } // end response success
+                    else   // unsuccessful response
+                    {
+                        if (response.code() == 400 || response.code() == 401) // received a response from server
+                        {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run()
+                                {
+                                    Toast.makeText(getActivity(), "Please verify username and password", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                        else
+                        {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run()
+                                {
+                                    Toast.makeText(getActivity(), "Network related error. Please try again", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+
+                        return;
+                    }
+
+                }
+            });
+
+
+            /****
+            FormBody.Builder formBuilder = new FormBody.Builder()
+                    .add("key", "123");
+
+            formBuilder.add("phone", "000000");
+
+            RequestBody formBody = formBuilder.build();
+
+            String urlEncoded = Uri.encode(urlAsString);
+            //RequestBody body = RequestBody.create(MEDIA_TYPE, urlEncoded);
+
+            Request request = new Request.Builder()
+                    .url(urlEncoded).post(formBody)
+                    .addHeader("Authorization", "Bearer " + token)
+                    .build();
+
+             ***/
+
+
+            /****
+             *  String reqBody = "grant_type=password&username=" + emailText.getText().toString()
+             + "&password=" + passwordText.getText().toString() ;
+
+             //grant_type=password&username=jds%40gmail.com&password=******";
+
+             final String ALLOWED_URI_CHARS = "@#&=*+-_.,:!?()/~'%";
+             String urlEncoded = Uri.encode(reqBody, ALLOWED_URI_CHARS);
+
+             RequestBody body = RequestBody.create(MEDIA_TYPE, urlEncoded);
+
+             final Request request = new Request.Builder()
+             .url(urlAsString)
+             .post(body)
+             .addHeader("Content-Type", "application/json")
+             .build();
+
+             */
+        }
+        else  // no token found
+        {
+            getActivity().finish(); // logout
+        }
+
+
+    }
+
+
+    private void drawMap(JSONArray json)
+    {
+        for (Integer i = 0; i < json.length(); i++)
+        {
+            try {
+                Double lat = json.getJSONObject(i).getDouble("Latitude");
+                Log.i("JSON = ", lat.toString());
+
+                Double lon = json.getJSONObject(i).getDouble("Longitude");
+                Log.i("JSON = ", lon.toString());
+
+               gMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon))); //.title("Marker in Lockport"));
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+
+
+
+
+
+        /*
+         func drawMap(result: AnyObject)
+    {
+        mapView.clear()
+
+        let resultAsArray = result as! NSArray
+
+        print("num bridges = \(resultAsArray.count)")
+
+        for bridge in resultAsArray
+        {
+            let b = bridge as! [String:AnyObject]
+
+            let bridgeLat = b["Latitude"] as? Double
+            let bridgeLon = b["Longitude"] as? Double
+
+            print("lat = \(String(describing: bridgeLat))")
+
+            // if lat pr lon invalid don't try to display it
+            if bridgeLat == nil || bridgeLon == nil { continue }
+            if bridgeLat! < -90 || bridgeLat! > 90 { continue }
+            if bridgeLon! < -180 || bridgeLon! > 180 { continue }
+
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2DMake(bridgeLat!, bridgeLon!)
+
+            let height = b["Height"] as? Double
+            if height != nil
+            {
+                marker.icon = UIImage(named: "marker_height2_orange.png")
+            }
+            else
+            {
+                marker.icon = UIImage(named: "marker_weight2_orange.png")
+            }
+
+             marker.map = mapView
+        }
+    }
+         */
+    }
+
+
+
+
+    /** SSL bypass **/
+    private static final TrustManager[] trustAllCerts = new TrustManager[] {
+            new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[]{};
+                }
+            }
+    };
+    private static final SSLContext trustAllSslContext;
+    static {
+        try {
+            trustAllSslContext = SSLContext.getInstance("SSL");
+            trustAllSslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static final SSLSocketFactory trustAllSslSocketFactory = trustAllSslContext.getSocketFactory();
+
+    public static OkHttpClient trustAllSslClient(OkHttpClient client) {
+
+        OkHttpClient.Builder builder = client.newBuilder();
+        builder.sslSocketFactory(trustAllSslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+        builder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+        return builder.build();
+    }
 
 
 }
