a85bc4a7bd62524443826c58c73c0d6e40c215a6
James Donner <jdsv650@yahoo.com>
2017-11-22 19:44:38 -0500

    UI for search fragment

--- app/src/main/java/com/jdsv650/bware/SearchFragment.java
+++ app/src/main/java/com/jdsv650/bware/SearchFragment.java
@@ -1,17 +1,20 @@
 package com.jdsv650.bware;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements View.OnClickListener {
 
 
     public SearchFragment() {
@@ -23,7 +26,24 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        Button button = (Button) view.findViewById(R.id.searchSubmitPressed);
+        button.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.searchSubmitPressed:
+
+                Toast.makeText(getActivity(), "Searching...", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 
 }
